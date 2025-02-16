3cfa80cec5222545e4b1c5be9b8211491e27dbc4
ryanjames-blip <136746530+ryanjames-blip@users.noreply.github.com>
2025-02-16 14:59:40 -0500

    Add files via upload
    complete Module 3

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Ryan DenBleyker
+// 2/16/2025
+// COP 2360
+
+public class Animal
+{
+    public string Name;
+    public bool LikesHumans;
+}
+public class Bunny : Animal
+{
+  public bool LikesCarrots;
+
+  public Bunny (string name, bool likesCarrots = false, bool likesHumans = false) {
+    Name = name;
+    LikesCarrots = likesCarrots;
+    LikesHumans = likesHumans; 
+  }
+
+  public void Run() {
+    Console.WriteLine("See {0} run!", Name);
+  }
+}
+
+public class Program() {
+    static void Main(string[] args) {
+        Bunny buns = new Bunny("buns", true, false);
+        buns.Run();
+    }
+}
