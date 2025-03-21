c22d26d5672e3449873ad8945723b23c82e93648
Ryan DenBleyker <ryandenbleyker@gmail.com>
2025-03-21 16:36:11 -0400

    Formatting and add function signatures.

--- Module7_Group_Project/Test.cs
+++ Module7_Group_Project/Test.cs
@@ -1,35 +1,37 @@
 
 public class Test {
+
     private static Dictionary<int, string> testDict = new Dictionary<int, string>();
+
     public void testDictionary() {
         Console.WriteLine("Choose From the options below to interact with the dictionary.\n");
         bool exitLoop = false;
         while (!exitLoop) {
             displayOptions();
-            int userInput = getChoice();
-            switch (userInput) {
+            //int userInput = getChoice();
+            switch (getChoice()) {
                 case 1:
-                    // POPULATE THE DICTIONARY
+                    // populate the dictionary
                     populate();
                     break;
                 case 2:
-                    // DISPLAY DICTIONARY CONTENTS
+                    // display dictionary contents
                     display();
                     break;
                 case 3:
-                    // REMOVE A KEY
+                    // remove a key
                     Console.WriteLine("Remove Key");
                     break;
                 case 4:
-                    // ADD A NEW KEY, VALUE PAIR
+                    // add a new key, value pair
                     Console.WriteLine("Add new key, value pair");
                     break;
                 case 5:
-                    // ADD A NEW VALUE TO SPECIFIED KEY
+                    // add a new value to the specified key
                     Console.WriteLine("Change a key's value");
                     break;
                 case 6:
-                    // SORT THE DICTIONARY
+                    // sort the dictionary
                     Console.WriteLine("Sort");
                     break;
                 case 7:
@@ -42,10 +44,12 @@ public class Test {
             }
         }
     }
+
     private static void displayOptions() {
         Console.WriteLine("1. Populate the dictionary.\n2. Display Contents\n3. Remove a Key.");
         Console.WriteLine("4. Add New Key and Value\n5. Add new Value to Key\n6. Sort\n7. Exit");
     }
+
     private static int getChoice() {
         int num;
         while(true) {
@@ -73,5 +77,21 @@ public class Test {
             Console.WriteLine($"Key: {entry.Key} Value: {entry.Value}");
         }
         Console.WriteLine();
+    }
+
+    private static void removeKey() {
+        // TODO: Remove a specified element
+    }
+
+    private static void addElement() {
+        // TODO: Add an element to the dictionary
+    }
+
+    private static void changeValue() {
+        // TODO: Change the value of a specified key
+    }
+
+    private static void sortDictionary() {
+        //TODO: Sort the dictionary
     }
 }
