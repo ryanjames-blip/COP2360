bc240b6e64a70692b3a3ccc3112e80c9b568241e
Ryan DenBleyker <ryandenbleyker@gmail.com>
2025-03-23 20:24:03 -0400

    Finish sort function. Add comments.

--- Module7_Group_Project/Test.cs
+++ Module7_Group_Project/Test.cs
@@ -1,7 +1,7 @@
 
 public class Test {
 
-    private static Dictionary<int, string> testDict = new Dictionary<int, string>();
+    private static Dictionary<int, string> employeeIDsAndNames = new Dictionary<int, string>();
 
     public void testDictionary() {
         Console.WriteLine("Choose From the options below to interact with the dictionary.\n");
@@ -35,10 +35,12 @@ public class Test {
                     sortDictionary();
                     break;
                 case 7:
+                    // set control variable to exit loop
                     Console.WriteLine("Exiting...");
                     exitLoop = true;
                     break;
                 default:
+                    // catch any incorrect int entries
                     Console.WriteLine("Please enter only digits 1 - 7:");
                     break;
             }
@@ -52,6 +54,7 @@ public class Test {
     }
 
     private static int getChoice() {
+        // catch incorrect type entries and return an int
         int num;
         while(true) {
             try {
@@ -59,7 +62,7 @@ public class Test {
                 break;
             }
             catch(Exception e) {
-                Console.WriteLine("Invalid Input");
+                Console.WriteLine(e.Message);
                 Console.WriteLine("Please try again: ");
             }
         }
@@ -67,30 +70,40 @@ public class Test {
     }
 
     private static void populate() {
-        if (testDict.Count() == 0) {
-            testDict.Add(3, "Ryan");
-            testDict.Add(2, "Ivory");
-            testDict.Add(1, "Ismael");
+        if (employeeIDsAndNames.Count() == 0) {
+            employeeIDsAndNames.Add(103, "Ryan");
+            employeeIDsAndNames.Add(102, "Kelsey");
+            employeeIDsAndNames.Add(101, "Ismael");
+            employeeIDsAndNames.Add(105, "Sydney");
+            employeeIDsAndNames.Add(104, "Ivory");
         }
         else {
             Console.WriteLine("Dictionary already includes test data.");
         }
+
+        Console.WriteLine("\nPopulated!\n");
     }
 
     private static void display() {
+        // loop through dictionary and display each key, value pair
         Console.WriteLine("\n---Displaying Dictionary Contents---\n");
-        foreach(var entry in testDict) {
-            Console.WriteLine($"Key: {entry.Key} Value: {entry.Value}");
+        Console.WriteLine("ID\tNAME");
+        Console.WriteLine("-------------");
+        foreach(var entry in employeeIDsAndNames) {
+            Console.WriteLine($"{entry.Key}\t{entry.Value}");
         }
         Console.WriteLine();
     }
 
     private static void removeKey() {
-        if (testDict.Count() == 0) {
+        // exit with message if dictionary is empty
+        if (employeeIDsAndNames.Count() == 0) {
             Console.WriteLine("Populate the dictionary with data first!");
         }
         else {
             Console.WriteLine("\nEnter a key to remove: ");
+
+            // check for correct type input
             int keyToRemove;
             while (true) {
                 try {
@@ -103,8 +116,9 @@ public class Test {
                 }
             }
 
-            if (testDict.ContainsKey(keyToRemove)) {
-                testDict.Remove(keyToRemove);
+            // check that key exists in the dictionary before removing
+            if (employeeIDsAndNames.ContainsKey(keyToRemove)) {
+                employeeIDsAndNames.Remove(keyToRemove);
                 Console.WriteLine($"Key: {keyToRemove} has been removed\n");
             }
             else {
@@ -115,6 +129,8 @@ public class Test {
 
     private static void addElement() {
         Console.WriteLine("\nEnter key to add: ");
+
+        // check for correct type input
         int keyToAdd;
         while (true) {
             try {
@@ -125,19 +141,29 @@ public class Test {
                 Console.WriteLine(e.Message);
             }
         }
-        if (testDict.ContainsKey(keyToAdd)) {
+        
+        // check that key does not already exist in the dictionary before adding
+        if (employeeIDsAndNames.ContainsKey(keyToAdd)) {
             Console.WriteLine("Dictionary already contains that key!");
         }
         else {
             Console.WriteLine($"Enter value associated with key {keyToAdd}: ");
-            string valueToAdd = Console.ReadLine();
-            testDict.Add(keyToAdd, valueToAdd);
-            Console.WriteLine($"{keyToAdd}, {valueToAdd} has been added!");
+            string? valueToAdd = Console.ReadLine();
+            if (valueToAdd is not null) {
+                employeeIDsAndNames.Add(keyToAdd, valueToAdd);
+            }
+            else {
+                employeeIDsAndNames.Add(keyToAdd, "Change this value!");
+                Console.WriteLine("Null entry\nPlaceholder value added.\n");
+            }
+            Console.WriteLine($"{keyToAdd}, {valueToAdd} has been added!\n");
         }
     }
 
     private static void changeValue() {
         Console.WriteLine("Enter the key to change: ");
+
+        // check for correct input type
         int keyToChange;
         while (true) {
             try {
@@ -148,21 +174,32 @@ public class Test {
                 Console.WriteLine(e.Message);
             }
         }
-        if (!testDict.ContainsKey(keyToChange)) {
+
+        // check that key exists in the dictionary before changing value
+        if (!employeeIDsAndNames.ContainsKey(keyToChange)) {
             Console.WriteLine("Key does not exist");
         }
         else {
-            Console.WriteLine($"Existing: {keyToChange} -- {testDict[keyToChange]}");
+            Console.WriteLine($"Existing: {keyToChange} -- {employeeIDsAndNames[keyToChange]}");
             Console.WriteLine("Enter new value: ");
-            string newValue = Console.ReadLine();
-            testDict[keyToChange] = newValue;
-            Console.WriteLine($"New: {keyToChange} -- {testDict[keyToChange]}");
+            string? newValue = Console.ReadLine();
+            if (newValue is not null) {
+                employeeIDsAndNames[keyToChange] = newValue;
+            }
+            else {
+                employeeIDsAndNames[keyToChange] = "Change this value!";
+                Console.WriteLine("Null entry\nPlaceholder value added.\n");
+            }
+            Console.WriteLine($"New: {keyToChange} -- {employeeIDsAndNames[keyToChange]}");
         }
     }
 
     private static void sortDictionary() {
-        foreach (KeyValuePair<int, string> entry in testDict.OrderBy(x => x.Key)) {
-            Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+        Console.WriteLine("ID\tNAME");
+        Console.WriteLine("-------------");
+        // display dictionary contents sorted by key
+        foreach (KeyValuePair<int, string> entry in employeeIDsAndNames.OrderBy(x => x.Key)) {
+            Console.WriteLine($"ID: {entry.Key}, Name: {entry.Value}");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# COP2360
-C# Programming
+## COP2360
+
+# C# Programming
+
+Learning how to code with C#
+
+# Module 7 Project
+
+All code is implemented in the Test.cs file
+Console application for testing dictionary functions
+
+The main switch statement controls the flow of the program. 
+Valid entries are 1 - 7
